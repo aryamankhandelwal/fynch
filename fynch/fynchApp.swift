f077d45db437c6319c5528774593a1ad987bfa08
Aryaman <aryaman99@gmail.com>
2026-04-08 15:19:37 -0400

    Initial Commit

---
+++ fynch/fynchApp.swift
@@ -0,0 +1,17 @@
+//
+//  fynchApp.swift
+//  fynch
+//
+//  Created by Aryaman on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct fynchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fynchTests/fynchTests.swift
@@ -0,0 +1,17 @@
+//
+//  fynchTests.swift
+//  fynchTests
+//
+//  Created by Aryaman on 4/8/26.
+//
+
+import Testing
+@testable import fynch
+
+struct fynchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
